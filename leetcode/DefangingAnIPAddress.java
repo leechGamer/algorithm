2604001d2660be1eb972c79880ef771db4e58b4c
geBAEBAE <gebae@olulo.io>
2020-04-19 12:20:29 +0900

    [LeetCode] DefangingAnIPAddress

---
+++ leetcode/DefangingAnIPAddress.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class  DefangingAnIPAddress {
+    public static String defangIPaddr(String address) {
+        address = address.replace(".", "[.]");
+        return address;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        String[] arr = new String[2];
+        arr = input.split("=");
+        String address = arr[1];
+        String result = defangIPaddr(address);
+        System.out.println(result);
+    }
+}
+
+
