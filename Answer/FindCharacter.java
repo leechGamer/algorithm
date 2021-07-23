bed568c5d4c83133e8b7969cd1c251bacb24a606
Ola <ruddmsdl000@naver.com>
2021-07-23 22:45:39 +0900

    Create FindCharacter.java

---
+++ Answer/FindCharacter.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    /*
+    Computercooler
+    c
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next().toUpperCase();
+        char target = scanner.next().toUpperCase().charAt(0);
+        int count = 0;
+        for (char i : input.toCharArray()) {
+            if (i == target) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
