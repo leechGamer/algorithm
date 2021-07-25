60a01dda2327be37f5480a19d18353350d85bc05
Ola <ruddmsdl000@naver.com>
2021-07-25 17:37:15 +0900

    Create RockScissor.java

---
+++ Answer/RockScissor.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+/*
+ */
+public class RockScissor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String[] A = new String[num];
+        String[] B = new String[num];
+        for (int i=0;i<num;i++) {
+            A[i] = scanner.next();
+        }
+
+        for (int i=0;i<num;i++) {
+            B[i] = scanner.next();
+        }
+
+        for (int i=0;i<num;i++) {
+            System.out.println(getWinner(A[i], B[i]));
+        }
+    }
+
+    static String getWinner(String A, String B) {
+        if (A.equals(B)) {
+            return "D";
+        } else {
+            if (A.equals("1")) {
+                if (B.equals("2")) {
+                    return "B";
+                } else if (B.equals("3")) {
+                    return "A";
+                }
+            } else if (A.equals("2")) {
+                if (B.equals("1")) {
+                 return "A";
+                } else if (B.equals("3")) {
+                    return "B";
+                }
+            } else if (A.equals("3")) {
+                if (B.equals("1")) {
+                    return "B";
+                } else if (B.equals("2")) {
+                    return "A";
+                }
+            }
+        }
+        return "";
+    }
+}
