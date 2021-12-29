8be68d1aef55c67a90137d89f716773b9ab187ae
Ola <ruddmsdl000@naver.com>
2021-12-29 19:38:36 +0900

    Create Main20221.java

---
+++ Answer/Main20221.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String text = s.next().toLowerCase();
+        char target = Character.toLowerCase(s.next().charAt(0));
+        int count = 0;
+        for (int i=0;i<text.length();i++) {
+            if (text.charAt(i) == target) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
