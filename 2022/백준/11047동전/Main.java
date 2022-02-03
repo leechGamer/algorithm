cba9311ca5de653df2468f487b7c9b7e37fda47b
Ola <ruddmsdl000@naver.com>
2022-02-03 22:04:37 +0900

    11047 동전

---
+++ 2022/백준/11047동전/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static int solution(int[] input, int total) {
+        int count = 0;
+        for (int i=input.length-1;i>-1;i--) {
+            int money = input[i];
+            if (money > total) {
+                continue;
+            }
+
+            if (total - money < 0) {
+                continue;
+            } else {
+                count += total / money;
+                total = total % money;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        int num = Integer.parseInt(stringTokenizer.nextToken());
+        int total = Integer.parseInt(stringTokenizer.nextToken());
+        int[] input = new int[num];
+        for (int i=0;i<num;i++) {
+            input[i] = (Integer.parseInt(String.valueOf(bufferedReader.readLine())));
+        }
+        bufferedReader.close();
+        System.out.print(solution(input, total));
+    }
+}
