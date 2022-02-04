d142e32bdd17b1cd64d0db22f277b5af123f857c
Ola <ruddmsdl000@naver.com>
2022-02-04 11:28:57 +0900

    Create Main.java

---
+++ 2022/2875대회OR인턴/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static int solution(int N, int M, int K) {
+        int result = 0;
+        result = Math.min(N / 2, M);
+
+        K -= N + M - (result * 3);
+
+        while (K > 0) {
+            result--;
+            K -= 3;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int M = s.nextInt();
+        int K = s.nextInt();
+
+        System.out.print(solution(N, M, K));
+    }
+}
