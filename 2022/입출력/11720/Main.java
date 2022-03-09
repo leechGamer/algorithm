7f25bc2255de91e02697ebd04faa9f8e0c9d2d82
Ola <ruddmsdl000@naver.com>
2022-03-09 14:25:34 +0900

    Update Main.java

--- 2022/입출력/11720/Main.java
+++ 2022/입출력/11720/Main.java
@@ -1,1 +1,13 @@
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        IntStream numbers = Arrays.stream(s.next().split("")).mapToInt(a -> Integer.parseInt(a));
+        System.out.println(numbers.sum());
+    }
+}
