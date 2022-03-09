8b46a57ce019391c65c20df2bc22631857dec51e
Ola <ruddmsdl000@naver.com>
2022-03-09 14:31:53 +0900

    Update Main.java

--- 2022/입출력/10950/Main.java
+++ 2022/입출력/10950/Main.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+class Main {
+     public static int solution(int a, int b) {
+         return a+b;
+     }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int T = s.nextInt();
+        for(int i=0;i<T;i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            System.out.println(solution(a, b));
+        }
+    }
+}
