6d6e0a95d14af0787a214dd362601c4ab8d88ec8
Ola <ruddmsdl000@naver.com>
2022-03-10 15:39:08 +0900

    Create Main.java

---
+++ 2022/입출력/2440/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        solution(n);
+    }
+
+    private static void solution(int n) {
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n-i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
