763ae54c411fb6d2faaf76fc969c0dea0753100e
g <ruddmsdl000@naver.com>
2022-08-02 08:32:22 +0900

    Update Solution.java

--- 2022/leetCode/Aug2/containsDuplicate/Solution.java
+++ 2022/leetCode/Aug2/containsDuplicate/Solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if (nums.length < 2) {
-            return false;
-        }
-        
-        Arrays.sort(nums);
-        int pre = nums[0];
-        for (int i=1;i<nums.length;i++) {
-            if (nums[i] == pre) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i=0;i<nums.length;i++) {
+            if (!set.add(nums[i])) {
                 return true;
             }
-            pre = nums[i];
         }
         return false;
     }
 }
 
+
 /**
-3분 소요
-Started        08/02/2022 08:14
-Time Submitted 08/02/2022 08:17	Accepted	30 ms 72.7 MB
+    3분 소요
+    Started        08/02/2022 08:14
+    Time Submitted 08/02/2022 08:17	Accepted	30 ms 72.7 MB
+*/
+
+
+/**
+    set으로 개선
+    7 ms	68.9 MB	
 */
