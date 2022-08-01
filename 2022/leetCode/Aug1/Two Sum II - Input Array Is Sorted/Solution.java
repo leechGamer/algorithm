eb33287ca6fdd570fa5158cfaf2c2336322f521e
g <ruddmsdl000@naver.com>
2022-08-02 07:46:28 +0900

    Update Solution.java

--- 2022/leetCode/Aug1/Two Sum II - Input Array Is Sorted/Solution.java
+++ 2022/leetCode/Aug1/Two Sum II - Input Array Is Sorted/Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int i = 0;
         int j = numbers.length - 1;
-         if(numbers[i] + numbers[j] == target){
+        if(numbers[i] + numbers[j] == target){
                 return new int[] {i + 1, j + 1};
-            }
-        while (numbers[i] + numbers[j] != target) {
+         }
+         while (numbers[i] + numbers[j] != target) {
             if (numbers[i] + numbers[j] < target) i++;
             else j--;
                 
