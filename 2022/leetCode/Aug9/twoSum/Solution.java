84046a948f9bfd2e5754629dc75a5bcb37eb95e9
g <ruddmsdl000@naver.com>
2022-08-09 20:39:32 +0900

    Create Solution.java

---
+++ 2022/leetCode/Aug9/twoSum/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+     Map<Integer, Integer> map = new HashMap();
+
+        for (int i=0;i<nums.length;i++) {
+            if (map.get(target - nums[i]) != null) {
+            int index = map.get(target - nums[i]);
+                return new int[]{i, index};
+            }  else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return new int[]{};
+    }
+}
